ae51b7438580b94cb53f5795c996ee2c081af9e0
BuildTools <unconfigured@null.spigotmc.org>
2019-08-19 20:58:30 +0200

    Countdown selects closest holiday #4

--- init.js
+++ init.js
@@ -1,3 +1,44 @@
+//Generate a holiday class
+class Holiday {
+    //Constructor
+    constructor(DateTime, CountdownNameString, CountdownFinishedString) {
+        this.DateTime = DateTime;
+        this.CountdownNameString = CountdownNameString;
+        this.CountdownFinishedString = CountdownFinishedString;
+    }
+
+    //Method for printing strings
+    PrintStrings() {
+        document.getElementById("above-text").innerHTML = this.CountdownNameString;
+        document.getElementById("done-container").innerHTML = this.CountdownFinishedString;
+    }
+}
+
+//Generate holiday objects
+let holidays = [
+new Holiday(new Date("Oct 4, 2019 13:15:00"), "Nedtelling til høstferien", "Høstferie nå!"), 
+new Holiday(new Date("Dec 21, 2019 10:25:00"), "Nedtelling til juleferien", "Juleferie nå!"),
+new Holiday(new Date("Feb 21, 2019 13:15:00"), "Nedtelling til vinterferien", "Vinterferie nå!"),
+new Holiday(new Date("Apr 3, 2019 13:15:00"), "Nedtelling til påskeferien", "Påskeferie nå!"),
+new Holiday(new Date("Jun 19, 2019 10:25:00"), "Nedtelling til sommerferien", "Sommerferie nå!")
+];
+
+function findClosestHoliday() {
+    let selected;
+    var lowestdistance = Infinity;
+    var currentDate = new Date();
+    holidays.forEach(function(holiday) {
+        var distance = holiday.DateTime.getTime() - currentDate.getTime();
+        if (distance < lowestdistance && distance > 24 * 60 * 60 * 1000 * -1) {
+            lowestdistance = distance;
+            selected = holiday;
+        }
+    });
+    return selected
+}
+
+
+
 //Init the clock
 var clock;
 $(document).ready(function() {
@@ -6,7 +47,11 @@ $(document).ready(function() {
     var currentDate = new Date();
 
     // Set some date in the future. høstferie
-    var futureDate  = new Date("Oct 4, 2019 13:15:00");
+    //var futureDate  = new Date("Oct 4, 2019 13:15:00");
+    var selholiday = findClosestHoliday();
+    var futureDate = selholiday.DateTime;
+    selholiday.PrintStrings();
+
     // Calculate the difference in seconds between the future and current date
     var diff = futureDate.getTime() / 1000 - currentDate.getTime() / 1000;
 
@@ -27,3 +72,5 @@ $(document).ready(function() {
         },
     });
 });
+
+
