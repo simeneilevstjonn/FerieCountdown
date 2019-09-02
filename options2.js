933cef8f677405fba78af8afdfb537095bce80c2
= <simene@lyse.net>
2019-09-02 09:10:10 +0200

    Using HourlyCounter for dayend

--- options2.js
+++ options2.js
@@ -27,3 +27,16 @@ function SetVariableForm(type) {
             break;
     }
 }
+
+class Options {
+    constructor(cdtype) {
+        if (cdtype == "w") {
+            document.getElementById("dayendoption").innerHTML("Arbeidsdagens Slutt");
+        }
+        this.cdtype = cdtype;
+    }
+    
+    //Holiday dates
+    a19 = new Date("Oct 4, 2019 13:15:00");
+
+}
